6992a1d26e91d1b2cb62c7bc3e6be2faef1000e0
reneechai <reneechai@tencent.com>
2021-10-26 11:33:05 +0800

    【Android】【TRTC】增加用户协议与隐私条例

--- Android/App/src/main/java/com/tencent/liteav/demo/LoginActivity.java
+++ Android/App/src/main/java/com/tencent/liteav/demo/LoginActivity.java
@@ -5,16 +5,25 @@ import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.TextPaint;
 import android.text.TextUtils;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.blankj.utilcode.util.ToastUtils;
 import com.tencent.liteav.demo.login.HttpLogicRequest;
 
 public class LoginActivity extends AppCompatActivity {
@@ -22,6 +31,8 @@ public class LoginActivity extends AppCompatActivity {
 
     private EditText mEditUserId;
     private Button   mButtonLogin;
+    private TextView mTvUserProtocol;
+    private CheckBox mCheckBoxProtocol;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -36,7 +47,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private void initView() {
         mEditUserId = (EditText) findViewById(R.id.et_userId);
+        mCheckBoxProtocol = findViewById(R.id.cb_protocol);
+        mTvUserProtocol = findViewById(R.id.tv_protocol);
         initButtonLogin();
+        updateStatement();
     }
 
     private void initButtonLogin() {
@@ -44,6 +58,10 @@ public class LoginActivity extends AppCompatActivity {
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!mCheckBoxProtocol.isChecked()) {
+                    ToastUtils.showShort(R.string.app_protocol_tip);
+                    return;
+                }
                 login();
             }
         });
@@ -72,6 +90,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login() {
         String userId = mEditUserId.getText().toString().trim();
+        if (TextUtils.isEmpty(userId)) {
+            ToastUtils.showShort(R.string.hint_user_id);
+            return;
+        }
         Log.d(TAG, "login: userId = " + userId);
         HttpLogicRequest.getInstance().login(userId, new HttpLogicRequest.ActionCallback() {
             @Override
@@ -95,6 +117,62 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
         finish();
+    }
+
+    private void updateStatement() {
+        final SpannableStringBuilder builder = new SpannableStringBuilder();
+        String protocolStart = getString(R.string.app_protocol_start);
+        String privacyProtocol = getString(R.string.app_privacy_protocol_detail);
+        String userAgreement = getString(R.string.app_user_agreement_detail);
+        String protocolAnd = getString(R.string.app_protocol_and);
+        builder.append(protocolStart);
+        builder.append(privacyProtocol);
+        builder.append(protocolAnd);
+        builder.append(userAgreement);
+
+        int privacyStartIndex = protocolStart.length();
+        int privacyEndIndex = privacyStartIndex + privacyProtocol.length();
+        ClickableSpan privacyClickableSpan = new ClickableSpan() {
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(getResources().getColor(R.color.app_color_blue));
+                ds.setUnderlineText(false);
+            }
+
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent("com.tencent.liteav.action.ktv.webview");
+                intent.putExtra("title", getString(R.string.app_privacy_protocol));
+                intent.putExtra("url", "https://web.sdk.qcloud.com/document/Tencent-RTC-Privacy-Protection-Guidelines.html");
+                startActivity(intent);
+            }
+        };
+        builder.setSpan(privacyClickableSpan, privacyStartIndex, privacyEndIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        int userAgreementStartIndex = privacyEndIndex + protocolAnd.length();
+        int userAgreementEndIndex = userAgreementStartIndex + userAgreement.length();
+        ClickableSpan userAgreementClickableSpan = new ClickableSpan() {
+            @Override
+            public void updateDrawState(@NonNull TextPaint ds) {
+                super.updateDrawState(ds);
+                ds.setColor(getResources().getColor(R.color.app_color_blue));
+                ds.setUnderlineText(false);
+            }
+
+            @Override
+            public void onClick(View widget) {
+                Intent intent = new Intent("com.tencent.liteav.action.ktv.webview");
+                intent.putExtra("title", getString(R.string.app_user_agreement));
+                intent.putExtra("url", "https://web.sdk.qcloud.com/document/Tencent-RTC-User-Agreement.html");
+                startActivity(intent);
+            }
+        };
+        builder.setSpan(userAgreementClickableSpan, userAgreementStartIndex, userAgreementEndIndex, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        mTvUserProtocol.setMovementMethod(LinkMovementMethod.getInstance());
+        mTvUserProtocol.setText(builder);
+        mTvUserProtocol.setHighlightColor(Color.TRANSPARENT);
     }
 
     private void initStatusBar() {
